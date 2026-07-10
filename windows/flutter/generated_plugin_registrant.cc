3e174cc2bd0304797254dfc218a90481f3c24ecd
Emre Yurtseven <emreyurtseven18@gmail.com>
2026-07-11 01:29:14 +0200

    chore: add windows support

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <audio_service_win/audio_service_win_plugin_c_api.h>
+#include <just_audio_windows/just_audio_windows_plugin.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AudioServiceWinPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AudioServiceWinPluginCApi"));
+  JustAudioWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("JustAudioWindowsPlugin"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+}
