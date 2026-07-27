b4f0c03406ac564889f668da07e5ad25b16660e5
Emre Yurtseven <emreyurtseven18@gmail.com>
2026-07-27 19:36:16 +0200

    fix: add isCoreLibraryDesugaringEnabled for push notifications package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -57,4 +58,10 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+    implementation("androidx.window:window:1.0.0")
+    implementation("androidx.window:window-java:1.0.0")
 }
